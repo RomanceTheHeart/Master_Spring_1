6597e6ee98fa7c21980d76a92a636fac38b7c425
Gerard E. Paul <gerardpaul@outlook.com>
2024-06-26 13:11:21 -0500

    Created the fourth module.

---
+++ Example_4/src/main/java/com/example/config/ProjectConfig.java
@@ -0,0 +1,50 @@
+package com.example.config;
+
+import com.example.beans.Vehicle;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+
+import java.util.Random;
+
+/*
+Spring @Configuration annotation is part of the Spring Core framework.
+Spring Configuration annotation indicates that the class has been definition
+methods. So Spring container can process the class and generate Spring Beans to be
+used in the application.
+ */
+@Configuration
+@ComponentScan(basePackages = "com.example.beans")
+public class ProjectConfig {
+
+    @Bean("Stylish")
+    Vehicle vehicle(){
+        var veh = new Vehicle();
+        veh.setName("Audi 8");
+        return veh;
+    }
+    @Bean("Luxury")
+    Vehicle vehicle_2(){
+        var veh = new Vehicle();
+        veh.setName("Buick");
+        return veh;
+    }
+    @Primary
+    @Bean("FastCars")
+    Vehicle vehicle_3(){
+        var veh = new Vehicle();
+        veh.setName("Ferrari");
+        return veh;
+    }
+
+    @Bean("Rich")
+    Vehicle vehicle_4(){
+        var veh = new Vehicle();
+        veh.setName("Mercedes");
+        return veh;
+    }
+
+}
+
---
+++ Example_4/src/main/java/com/example/main/Example_4.java
@@ -0,0 +1,30 @@
+package com.example.main;
+
+import com.example.beans.Vehicle;
+import com.example.config.ProjectConfig;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class Example_3 {
+    public static void main(String[] args) {
+// This object is not tracked inside the spring context.
+        //this line is responsible for initializing  and keeping track of all the beans.
+
+
+        var context = new AnnotationConfigApplicationContext(ProjectConfig.class);
+
+        Vehicle veh = context.getBean(Vehicle.class);
+        veh.PrintVehicleName(veh.getName());
+
+        Vehicle vehicle = context.getBean("Rich",Vehicle.class);
+        vehicle.PrintVehicleName(vehicle.getName());
+
+
+        /*
+        We don't need to do any explicit casting while fetching a bean from context.
+        Spring is smart enough to look for a bean of the type you requested in its context.
+        If such a bean doesn't exist, Spring will throw an exception.
+        Ex.
+         */
+
+    }
+}
